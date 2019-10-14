94ed428c9543cba65b5eb7a96f420b8d90eab49b
Holfe <wvcneto@gmail.com>
2019-10-14 18:08:49 -0300

    UI implemented

--- index.js
+++ index.js
@@ -1,10 +1,80 @@
 //Imports
-import React, { Component } from 'react';
-import { AppRegistry, View, Text, Button } from 'react-native';
+import React, {Component} from 'react';
+import {AppRegistry, View, Text, Button, Image} from 'react-native';
 
 //Style
 
+const Style = {
+  mainView: {
+    flex: 1,
+  },
+  btn: {
+    width: 100,
+  },
+  btnView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 10,
+  },
+  resultView: {
+    alignItems: 'center',
+  },
+  textResult: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'red',
+  },
+  iconResultView: {
+    alignItems: 'center',
+    margin: 20,
+  },
+  textResultView: {
+    fontSize: 20,
+  },
+};
+
 //Components
+
+class Top extends Component {
+  render() {
+    return (
+      <View>
+        <Image source={require('./img/jokenpo.png')} />
+      </View>
+    );
+  }
+}
+
+class Result extends Component {
+  render() {
+    //this.props.choice
+    //this.props.player
+    if (this.props.choice === 'Rock') {
+      return (
+        <View style={Style.iconResultView}>
+          <Text style={Style.textResultView}>{this.props.player}</Text>
+          <Image source={require('./img/pedra.png')} />
+        </View>
+      );
+    } else if (this.props.choice === 'Paper') {
+      return (
+        <View style={Style.iconResultView}>
+          <Text style={Style.textResultView}>{this.props.player}</Text>
+          <Image source={require('./img/papel.png')} />
+        </View>
+      );
+    } else if (this.props.choice === 'Scissors') {
+      return (
+        <View style={Style.iconResultView}>
+          <Text style={Style.textResultView}>{this.props.player}</Text>
+          <Image source={require('./img/tesoura.png')} />
+        </View>
+      );
+    } else {
+      return false;
+    }
+  }
+}
 
 //Passando valores para components utilizando {props}
 
@@ -38,27 +108,27 @@ class rpsgame extends Component {
         result = 'Draw';
       }
       if (userChoice === 'Paper') {
-        result = 'Won';
+        result = 'You Won';
       }
       if (userChoice === 'Scissors') {
-        result = 'Lost';
+        result = 'You Lost';
       }
     } else if (computerChoice === 'Paper') {
       if (userChoice === 'Rock') {
-        result = 'Lost';
+        result = 'You Lost';
       }
       if (userChoice === 'Paper') {
         result = 'Draw';
       }
       if (userChoice === 'Scissors') {
-        result = 'Won';
+        result = 'You Won';
       }
     } else if (computerChoice === 'Scissors') {
       if (userChoice === 'Rock') {
-        result = 'Won';
+        result = 'You Won';
       }
       if (userChoice === 'Paper') {
-        result = 'Lost';
+        result = 'You Lost';
       }
       if (userChoice === 'Scissors') {
         result = 'Draw';
@@ -74,28 +144,39 @@ class rpsgame extends Component {
 
   render() {
     return (
-      <View>
-        <Text>Computer: {this.state.computerChoice}</Text>
-        <Text>Player: {this.state.userChoice}</Text>
-        <Text>Resultador: {this.state.result}</Text>
-        <Button
-          title="Rock"
-          onPress={() => {
-            this.jokenpo('Rock');
-          }}
-        />
-        <Button
-          title="Paper"
-          onPress={() => {
-            this.jokenpo('Paper');
-          }}
-        />
-        <Button
-          title="Scissors"
-          onPress={() => {
-            this.jokenpo('Scissors');
-          }}
-        />
+      <View style={Style.mainView}>
+        <Top />
+        <View style={Style.btnView}>
+          <View style={Style.btn}>
+            <Button
+              title="Rock"
+              onPress={() => {
+                this.jokenpo('Rock');
+              }}
+            />
+          </View>
+          <View style={Style.btn}>
+            <Button
+              title="Paper"
+              onPress={() => {
+                this.jokenpo('Paper');
+              }}
+            />
+          </View>
+          <View style={Style.btn}>
+            <Button
+              title="Scissors"
+              onPress={() => {
+                this.jokenpo('Scissors');
+              }}
+            />
+          </View>
+        </View>
+        <View style={Style.resultView}>
+          <Text style={Style.textResult}>{this.state.result}</Text>
+          <Result choice={this.state.userChoice} player="Player" />
+          <Result choice={this.state.computerChoice} player="Computer" />
+        </View>
       </View>
     );
   }
