27b5b7a8785838baa0a9a035fd933fb24b5648ba
Holfe <wvcneto@gmail.com>
2019-10-14 16:17:04 -0300

    Upgrade Game Logic

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Imports
-import React, {Component} from 'react';
-import {AppRegistry, View, Text, Button} from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, View, Text, Button } from 'react-native';
 
 //Style
 
@@ -9,21 +9,35 @@ import {AppRegistry, View, Text, Button} from 'react-native';
 //Passando valores para components utilizando {props}
 
 class rpsgame extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
 
-    this.state = {choice: ''};
+    this.state = { userChoice: '', computerChoice: '' };
   }
 
-  jokenpo(userChoice) {
-    this.setState({choice: userChoice}); // alterando o state de acordo com a chamada
+  jokenpo(choice) {
+    var rand = Math.floor(Math.random() * 3);
+    var compChoice = '';
+
+    switch (rand) {
+      case 0:
+        compChoice = 'Rock';
+        break;
+      case 1:
+        compChoice = 'Paper';
+        break;
+      case 2:
+        compChoice = 'Scissors';
+        break;
+    }
+    this.setState({userChoice: choice, computerChoice: compChoice}); // alterando o state de acordo com a chamada
   }
 
   render() {
     return (
       <View>
-        <Text>Computer: </Text>
-        <Text>Player: {this.state.choice}</Text>
+        <Text>Computer: {this.state.computerChoice}</Text>
+        <Text>Player: {this.state.userChoice}</Text>
         <Text>Resultador: </Text>
         <Button
           title="Rock"
